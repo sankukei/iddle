e310ddcaeededf51753968cf370cc5c74543d944
sanku <sankukei@gmail.com>
2024-07-28 17:47:51 +0200

    add localstorage save and fetch

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 let game_data = {
-    coockie : 0,
+    cookie : 0,
 }
+
 
 function set_value() {
 
-    let cook = document.getElementById("coockie")
-    let tmp = game_data.coockie
+    let cook = document.getElementById("cookie")
+    let tmp = game_data.cookie
     cook.innerText = tmp
 }
 
 function increment() {
 
-    let cook = document.getElementById("coockie");
-    game_data.coockie++
-    cook.innerText = game_data.coockie
+    const cook = document.getElementById("cookie");
+    console.log(game_data)
+
+    game_data.cookie++
+    cook.innerText = game_data.cookie
 }
 
+let save_game_loop = setInterval(() => {
+    localStorage.setItem("game_data", JSON.stringify(game_data))
+}, 150)
+
+let save_game = JSON.parse(localStorage.getItem("game_data"))
+if (save_game !== null) {
+    game_data = save_game
+}
+
+set_value()
