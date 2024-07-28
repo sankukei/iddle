333807fa9227b3287d4b6828d9b11552fb7e2bd6
sanku <sankukei@gmail.com>
2024-07-28 17:07:21 +0200

    blank

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+let game_data = {
+    coockie : 0,
+}
 
+function set_value() {
+
+    let cook = document.getElementById("coockie")
+    let tmp = game_data.coockie
+    cook.innerText = tmp
+}
 
 function increment() {
 
     let cook = document.getElementById("coockie");
-    let value = cook.innerText
-    value++;
-    cook.innerText = value
+    game_data.coockie++
+    cook.innerText = game_data.coockie
 }
+
